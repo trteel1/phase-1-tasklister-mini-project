bd638be9f621f215cf08c8629d58e011d7e4a30b
trteel1 <tateelamino@gmail.com>
2023-01-26 19:56:31 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const newTaskDescription = document.querySelector(`#new-task-description`)
+  const form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    toDoTask(newTaskDescription.value)
+    form.reset()
+  })
+  
+
+
+function toDoTask(task){
+  const listItems = document .createElement(`li`)
+  listItems.textContent = `${task}`
+  const btn = document.createElement('button')
+  btn.textContent = " x"
+  btn.addEventListener('click', (e)=>{
+    e.target.parentNode.remove()
+  })
+  document.querySelector('#tasks').appendChild(listItems).appendChild(btn)
+}
